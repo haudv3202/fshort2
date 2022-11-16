90519f51756e6f4c7334942572d3637bf627ccd1
cuongdeptraivkl <maithanhcuong251003@gmail.com>
2022-11-16 22:14:02 +0700

    o lala

--- global.php
+++ global.php
@@ -8,7 +8,7 @@ function dd(){
 
 
 
-const BASE_URL = "http://localhost/fshort2/"; // url đến đường dẫn
+const BASE_URL = "http://localhost:8080/fshort2/"; // url đến đường dẫn
 const SITE_URL = BASE_URL . "site/index.php"; // đường dẫn đến trang chuyển trang
 const ADMIN_BASE = BASE_URL . 'admin/'; // đường dẫn vào admin
 
