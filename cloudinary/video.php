4b6d08d91a64b12fcb16ed20906f4a9ff05f3fea
haudv3202 <haudang02.it@gmail.com>
2022-11-24 00:52:46 +0700

    update fogot google

---
+++ cloudinary/video.php
@@ -0,0 +1,44 @@
+<!--<form action="" method="post" enctype="multipart/form-data">-->
+<!--    <input type="file" name="image_1" id="">-->
+<!--    <input type="submit" name="submit" value="Update">-->
+<!--</form>-->
+
+<?php 
+
+require_once "vendor/autoload.php";
+use Cloudinary\Configuration\Configuration;
+use Cloudinary\Api\Upload\UploadApi;
+Configuration::instance([
+    'cloud' => [
+      'cloud_name' => 'dnbbehk5b', 
+      'api_key' => '377518485928239', 
+      'api_secret' => 'FEQQeg4Rks0yeZ8I28Hn8GOUuC4'],
+    'url' => [
+      'secure' => true]]);
+
+    //   (new UploadApi())->upload($path); img
+    $path =  null;
+    if(isset($_POST['sb_video'])){
+        $a = $_FILES['videohome'];
+        $title = $_POST['title'];
+            $path = $a['tmp_name'];
+            $namevideo = $a['name'];
+            $name = explode(".",$namevideo);
+            $data = (new UploadApi())->upload($path, [
+                    'resource_type' => 'video',
+                    'public_id' => 'Fshort/video/' . $name[0],
+                    'chunk_size' => 6000000,
+                    'eager' => [
+                        ['width' => 720, 'height' => 1280, 'crop' => 'pad'],
+                        ['width' => 160, 'height' => 100, 'crop' => 'crop', 'gravity' => 'south']],
+                    'eager_async' => true,
+                    'eager_notification_url' => 'https://mysite.example.com/notify_endpoint']
+            );
+            echo $title;
+            echo $data['url'];
+//            new_post($title,$data['url'],1,$_SESSION['info']['id']);
+
+
+    }
+
+?>
