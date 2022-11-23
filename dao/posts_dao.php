e8f2b429c5a781d2dc25344670aeeb563e3ea7ef
haudv3202 <haudang02.it@gmail.com>
2022-11-24 00:53:03 +0700

    update fogot google

--- dao/posts_dao.php
+++ dao/posts_dao.php
@@ -16,3 +16,13 @@ function post($id){
     $sql = "SELECT * FROM posts WHERE id = '$id'";
     return pdo_query_all($sql);
 }
+
+function new_post($title,$link,$id_cate,$id_acc){
+    $sql = "INSERT INTO posts VALUES (NULL,'$title','$link',CURRENT_TIMESTAMP(),NULL,'$id_acc','$id_cate',NULL,0,0,0)";
+    return pdo_execute($sql);
+}
+
+function all_post(){
+    $sql ="SELECT * FROM posts";
+    return pdo_query_all($sql);
+}
