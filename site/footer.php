185098774d2d9b815952ca5c71cd55e12a078560
LeeHairDev <leehairdev.it@gmail.com>
2022-11-24 22:12:15 +0700

    up phần chi tiết trang home

--- site/footer.php
+++ site/footer.php
@@ -1,14 +1,14 @@
 <?php
 ?>
 
-<!-- <div id="logo_loader">
+<div id="logo_loader">
 </div>
 <div class="loader">
 </div>
 <div id="text_loader">
-</div> -->
+</div>
 <script src="../public/setting/js/home.js"></script>
 </body>
 </html>
 
-    
+    3 
