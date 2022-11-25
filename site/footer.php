a44f1665ecda2b451d386708afd64d156b1b53c3
haudv3202 <110585224+haudv3202@users.noreply.github.com>
2022-11-25 13:36:39 +0700

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -7,14 +7,9 @@
 </div>
 <div id="text_loader">
 </div>
-<<<<<<< HEAD
+
 <script src="../public/setting/js/ajaxcmt.js"></script>
-<script src="../public/setting/js/home.js"></script>
-</body>
-</html>
-=======
 
 </body>
 </html>
 
->>>>>>> 1319b0f282b3ef7486367f606fe4f5aab8827ccd
