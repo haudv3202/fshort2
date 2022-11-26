ada10619a814b65e9ab347f12d06110a5526a423
LeeHairDev <leehairdev.it@gmail.com>
2022-11-27 00:06:03 +0700

    up

---
+++ .history/public/setting/js/ajaxcmt_20221126161614.js
@@ -0,0 +1,3 @@
+    $(document).ready(function(){
+        $("#binhluan").load("View/Binhluan/binhluan.php", {idpro: ${item.id_post}});
+    });
---
+++ .history/site/footer_20221126163927.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<div id="logo_loader">
+</div>
+<div class="loader">
+</div>
+<div id="text_loader">
+</div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="../"></script>
+
+</body>
+</html>
+
