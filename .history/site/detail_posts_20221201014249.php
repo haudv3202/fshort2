aab5c51d2d1dbde9a0caa6d3deaabb5e6f9b91a2
LeeHairDev <leehairdev.it@gmail.com>
2022-12-01 01:43:02 +0700

    up

---
+++ .history/site/detail_posts_20221201014249.php
@@ -0,0 +1,221 @@
+
+<?php
+                
+                if(isset($_SESSION['info'])){
+                    $user = $_SESSION['info'];
+                  ?>
+      <div class="detail_user">
+        <div class="avatar_name_detail_user">
+          <div class="mt-3 avatar_detail_user">
+            <img
+              class="img"
+            src="<?php echo $_SESSION['info']['link_avatar'];?>"
+              alt=""
+            />
+          </div>
+          <div class="name_detail_user">
+            <div class="id_user_detail">
+
+              <h2><?php echo $_SESSION['info']['name'];?></h2>
+
+              <div class="share_user_detail">
+                <i class='bx bx-share bx-flip-horizontal share_user_detail_i' ></i>
+                <ul class="coplink_user_detail">
+                  <li>
+                    <a href=""
+                      ><i class="bx bxl-facebook"></i>
+                      <p>Chia Sẻ Với Facebook</p></a
+                    >
+                  </li>
+                  <li>
+                    <a href=""
+                      ><i class="bx bx-link-alt"></i>
+                      <p>Sao chép liên kết</p></a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="name_user_detail">
+              <h3>#<?php echo $_SESSION['info']['id'];?><</h3>
+            </div>
+            <div class="edit_profile_user_detail">
+              <button class="btn_edit_user">
+                <i class="bx bxs-edit"></i><a href="?setting">Sửa hồ sơ</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="ms-4 mt-3 contact_user_detail">
+          <div class="Interactive">
+            <h5 class="number_user_fl">1</h5>
+            <span class="text_interactive">Đang Follow</span>
+          </div>
+          <div class="Interactive">
+            <h5 class="number_user_fl">1.5M</h5>
+            <span class="text_interactive">Follower</span>
+          </div>
+          <div class="Interactive">
+            <h5 class="number_user_fl">100M</h5>
+            <span class="text_interactive">Thích</span>
+          </div>
+        </div>
+        <div class="Story_user_detai mt-2">
+          <span class="ms-4 text-secondary fs-5">Chưa có tiểu sử.</span>
+        </div>
+        <?php
+                
+                }
+                    
+                  ?>
+        <div class="Post_user_detail">
+          <div class="title_video_user_detail">
+            <span class="btn_video fs-5 fw-normal"
+              ><a href="?detail_video">Video</a></span
+            >
+            <span class="btn_video fs-5 fw-normal"
+              ><a href="?detail_posts">Bài Viết</a></span
+            >
+            <span class="btn_video fs-5 fst-normal"
+              ><i class="bx bxs-lock-alt"></i
+              ><a href="?detail_video">Đã thích</a></span
+            >
+          </div>
+          <div class="Posts_user">
+            <!-- Render  -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+    <div id="product_sp">
+    
+     <div class="product_content_sp">
+         <div class="content">
+             <div class="slide">
+               <div class="icon_box">
+                 <div class="icon">
+                   <button onclick="left(-1)" class="left"><i class='bx bx-chevron-left' ></i></button>
+                   </div>
+                     <div class="icon1">
+                    <button onclick="left(+1)" class="right"><i class='bx bx-chevron-right' ></i></button>
+                    </div>
+               </div>
+                 <div class="img_content_posts">
+                  <div class="img_posts">
+                  <img id="anh" src="../public/setting/img/images.jpg" alt="" />
+                  </div>
+                  </div>
+                 </div>
+                 <div class="ms-3 text_posts">
+                     <div class="avatar_name_posts_user">
+                     <div class="avatar">
+                       <img src="../public/setting/img/chup-anh-hoang-hon-bang-smartphone_800x450.jpg" alt="">
+                     </div>
+                     <div class="name">
+                        <span class="fs-5 fw-normal">Lê Sỹ Hải</span>
+                    <p class="text-secondary">Đã đăng vào 5 ngày trước</p>
+                     </div>
+                     </div>
+                       <div class="comment_posts_user">
+                       <div class="text_title_posts">
+                          <span class="text_title"></span>
+                           <div class="user_other_comment_title">
+                          <div class="avatar_name_user_other_comment">
+                              <div class="avatar">
+                         <img src="../public/setting/img/bi-quyet-chup-anh-hoang-hon-dep-Binhminhdigital(1).jpg" alt="">
+                        </div>
+                           <div class="name">
+                          <span class="name_title">Lê Sỹ Hải</span>
+                          <span class="author_posts_detail">.Tác Giả</span>
+                       </div>
+                        </div>
+                        <div class="text_title">
+                            <span>Đây là nội dung chi tiết của bạn mong bạn có những trải nghiệm tost nhất về sản phẩm của chúng tôi mọi góp ý của bạn sẽ được chúng tôi ghi nhận vì đây là sản phẩm đầu tay nên chúng tôi còn nhiều thiếu sót rất mong các bạn thông cảm</span>
+                        </div>
+                        <div class="time_title_comment_posts">
+                            <span class="ms-5 fw-normal d-flex align-items-center gap-1">5 ngày trước</span>
+                        </div>
+                       </div>
+                       </div>
+                     <div class="user_comment_box">
+                        <div class="user_other_comment">
+                          <div class="avatar_name_user_other_comment">
+                              <div class="avatar">
+                         <img src="../public/setting/img/bi-quyet-chup-anh-hoang-hon-dep-Binhminhdigital(1).jpg" alt="">
+                        </div>
+                           <div class="name">
+                          <span class="fs-7 fw-normal">Bùi Mạnh Cường</span>
+                       </div>
+                        </div>
+                        <div class="content_other_comment ms-5">
+                            <span>Chúc phần mềm của các bạn phát triển hơn nhé</span>
+                        </div>
+                        <div class="time_comment_posts">
+                            <span class="ms-5 fw-normal d-flex align-items-center gap-1">Hôm qua lúc 19:30</span>
+                        </div>
+                       </div>
+                      <div class="user_other_comment">
+                        <div class="avatar_name_user_other_comment">
+                            <div class="avatar">
+                       <img src="../public/setting/img/chup-anh-hoang-hon-bang-smartphone_800x450.jpg" alt="">
+                      </div>
+                         <div class="name">
+                        <span class="fs-7 fw-normal">Đặng Văn Hậu</span>
+                     </div>
+                      </div>
+                      <div class="content_other_comment">
+                          <span>Sản phẩm của bạn dùng khá ổn cố gắng phát huy nhé</span>
+                      </div>
+                      <div class="time_comment_posts">
+                          <span class="ms-5 fw-normal d-flex align-items-center gap-1">3 ngày trước</span>
+                      </div>
+                     </div>
+                      <div class="user_other_comment">
+                        <div class="avatar_name_user_other_comment">
+                            <div class="avatar">
+                       <img src="../public/setting/img/Ngan-Ha25.jpg" alt="">
+                      </div>
+                         <div class="name">
+                        <span class="fs-7 fw-normal">Đậu Phương thảo</span>
+                     </div>
+                      </div>
+                      <div class="content_other_comment ms-5">
+                          <span>Tôi đã có trải nghiệm rất tuyệt vời với sản phẩm này cảm ơn các bạn rất nhiều vì đã đem đến cho chúng tôi một sản phẩm rất chất lượng</span>
+                      </div>
+                      <div class="time_comment_posts">
+                          <span class="ms-5 fw-normal d-flex align-items-center gap-1">5 ngày trước</span>
+                      </div>
+                     </div>
+                     </div>
+
+                  </div>
+                  <!-- <hr class="text-secondary"> -->
+                  <div class="interactive_posts_detail">
+                      <div class="icon_interactive_user">
+                        <div class="heart_colors_posts">
+                            <i id="heart-icon" class='bx bx-heart' onclick="clickheartposts()"></i>
+                        </div>
+                    <i id="chat-icon" class='bx bx-message-rounded bx-flip-horizontal' onclick="comment()"></i>
+                    <i class='bx bxl-telegram'></i>
+                  </div>
+                  <div class="likes_posts_user">
+                    <span class="number_view_posts">0</span><span> lượt thích</span>
+                  </div>
+                  </div>
+                  <!-- <hr class="text-secondary post_comment_ip"> -->
+                  <form class="input_comment_user">
+                    <input class="ip_cmt" type="text" oninput="checkcomment_ip()" placeholder="Viết bình luận của bạn">
+                    <button class="button_comment" >Đăng</button>
+                  </form>
+                     </div>
+             </div>
+            </div>
+            <span id="close" class=""><i onclick="showProduct()" class="bx bx-x text-white fw-normal fs-1 position-absolute top-0 right-0"></i></span>
+       </div>
+</div>
+    </div>
+
