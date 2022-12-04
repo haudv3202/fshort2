c676155eb388839397ec7e04097f05f9ecf6d1e5
haudv3202 <haudang02.it@gmail.com>
2022-12-04 22:28:49 +0700

    update profile other

--- site/detail_posts_user_other.php
+++ site/detail_posts_user_other.php
@@ -1,7 +1,5 @@
-<?php
-                if(isset($_SESSION['info'])){
-                    $user = $_SESSION['info'];
-                  ?>
+
+
       <div class="detail_user">
         <div class="avatar_name_detail_user">
           <div class="mt-3 avatar_detail_user">
@@ -37,7 +35,7 @@
             <div class="name_user_detail">
               <h2>#<?php echo $user['id']  ?></h2>
             </div>
-              <?php if($_SESSION['info']['id'] == $user['id']){?>
+              <?php if(isset($_SESSION['info']['id']) && ($_SESSION['info']['id'] == $user['id'])){?>
             <div class="edit_profile_user_detail">
               <button class="btn_edit_user">
                 <i class="bx bxs-edit"></i><a href="?setting">Sửa hồ sơ</a>
@@ -63,11 +61,6 @@
         <div class="Story_user_detai mt-2">
           <span class="ms-4 text-secondary fs-5">Chưa có tiểu sử.</span>
         </div>
-        <?php
-                
-                }
-                    
-                  ?>
         <div class="Post_user_detail">
          <div class="title_video_user_detail">
             <span class="btn_video fs-5 fw-normal"
