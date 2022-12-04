40ef45cb2ec80cd146d1a94031bc977b6521166e
haudv3202 <haudang02.it@gmail.com>
2022-12-04 22:49:44 +0700

    fix conflit

--- public/setting/js/home.php
+++ public/setting/js/home.php
@@ -893,7 +893,7 @@
             position: "right", // `left`, `center` or `right`
             stopOnFocus: true, // Prevents dismissing of toast on hover
             style: {
-                background: "linear-gradient(to right, #00b09b, #96c93d)",
+                background: "linear-gradient(to right,#e66465, #9198e5)",
                 borderRadius:'20px',
             },
             onClick: function(){} // Callback after click
