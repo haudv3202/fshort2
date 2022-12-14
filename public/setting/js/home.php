33dcb41724db2300479319fd74652e5fe2111438
cuongdeptraivkl <cuongbmph22166@fpt.edu.vn>
2022-12-14 15:10:48 +0700

    updateUi

--- public/setting/js/home.php
+++ public/setting/js/home.php
@@ -111,7 +111,9 @@
 
                     `</div>
         </div>
-
+        <div class="content_video_page">
+            <p>${item.title}</p>
+        </div>      
     <div class="links_video">
 
   <a href="?detail_video_mini&id_post=${item.id_post}" ><video  width="100%" onplay="getCurTime();" id="Video" controls type="video/mp4" loop
@@ -134,10 +136,7 @@
             <p>${item.likes} lượt thích</p>
         </div>
     </div>
-    <div class="content_video_page">
-        <p><b>#manhcuongEntertaiment</b></p>
-        <p>${item.title}</p>
-    </div>
+   
 <div class="comment_video_page`+`${item.id_post}`+` display" style='display: none;'>
 ` + Comments_video + `
 </div>
